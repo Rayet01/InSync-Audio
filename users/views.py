6d45adfe01cdfd64a21d0a1ba193f3091caf56a8
Sol Rayet <ge9i3fg8@students.codeinstitute.net>
2024-12-05 10:19:41 +0000

    Create custom user registration form with mandatory email field

--- users/views.py
+++ users/views.py
@@ -2,17 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}! You can now log in.')
-            return redirect('login')  # Redirect to the login page
+            return redirect('login')  # Redirect to login after successful signup
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
 @login_required
