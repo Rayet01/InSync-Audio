83723450cf8d301279e57019d308a27a48fd81ed
Sol Rayet <ge9i3fg8@students.codeinstitute.net>
2024-12-06 10:39:17 +0000

    Deployment commit

--- insync/settings.py
+++ insync/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rayet01-insyncaudio-g50u6tfz55b.ws.codeinstitute-ide.net', 'insyncaudio-8e866be95d5d.herokuapp.com']
 
