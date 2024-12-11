d92c810179713c59602d79e02b358f0c456798e8
Sol Rayet <ge9i3fg8@students.codeinstitute.net>
2024-12-11 16:20:23 +0000

    Update settings.py

--- insync/urls.py
+++ insync/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     
     # Auth routes
     path('accounts/', include('allauth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
